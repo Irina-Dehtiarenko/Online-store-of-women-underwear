5486721a6ccbf479085f90e3c70dcb98bbb89b81
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-10-29 20:35:28 +0200

    created the function of changing steps delivery and ordering after click on the dot

---
+++ main.js
@@ -0,0 +1,59 @@
+const allDotsDelivery = [...document.querySelectorAll(".delivery .dot")];
+
+const allDeliveryRules = [
+  ...document.querySelectorAll(".delivery-rules div.rules"),
+];
+
+const allDotsOrder = [...document.querySelectorAll(".how-to-order .dot")];
+
+const allOrderSteps = [...document.querySelectorAll(".order-steps div.step")];
+
+const changeDescription = (e) => {
+  //   console.log(e.target);
+  allDotsDelivery.forEach((dot) => {
+    dot.classList.remove("active-dot");
+  });
+
+  const dotActive = e.target;
+  dotActive.classList.add("active-dot");
+
+  console.log(dotActive);
+  allDeliveryRules.forEach((rules) => {
+    rules.classList.add("hidden");
+  });
+  if (e.target === allDotsDelivery[1]) {
+    allDeliveryRules[1].classList.remove("hidden");
+  } else if (e.target === allDotsDelivery[2]) {
+    allDeliveryRules[2].classList.remove("hidden");
+  } else if (e.target === allDotsDelivery[0]) {
+    allDeliveryRules[0].classList.remove("hidden");
+  }
+  //   e.target === allDotsDelivery[1]
+};
+allDotsDelivery.forEach((dot) => {
+  dot.addEventListener("click", changeDescription);
+});
+
+const changeDescriptionOrder = (e) => {
+  allDotsOrder.forEach((dot) => {
+    dot.classList.remove("active-dot");
+  });
+
+  const dotActive = e.target;
+  dotActive.classList.add("active-dot");
+
+  allOrderSteps.forEach((rules) => {
+    rules.classList.add("hidden");
+  });
+  if (e.target === allDotsOrder[1]) {
+    allOrderSteps[1].classList.remove("hidden");
+  } else if (e.target === allDotsOrder[2]) {
+    allOrderSteps[2].classList.remove("hidden");
+  } else if (e.target === allDotsOrder[0]) {
+    allOrderSteps[0].classList.remove("hidden");
+  }
+  //   e.target === allDotsDelivery[1]
+};
+allDotsOrder.forEach((dot) => {
+  dot.addEventListener("click", changeDescriptionOrder);
+});
